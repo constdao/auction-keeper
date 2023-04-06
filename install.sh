e7818bd1e3342cc987a7cd4b0552c516a4d233c6
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2023-04-06 13:56:47 +0800

    chore: add python3.6 command check, venv only use python 3.6

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@ cd "$(dirname "$0")"
 
 set -e
 
+if [ ! "$(command -v python3.6)" ]; then
+    echo "Python 3.6 is required to run this script."
+fi
+
 rm -rf _virtualenv
-virtualenv _virtualenv
+virtualenv _virtualenv --python="$(which python3.6)"
 source _virtualenv/bin/activate
 
 # The advantage of using this method, in contrary to just calling `pip install -r requirements.txt` several times,
 # is that it can detect different versions of the same dependency and fail with a "Double requirement given"
 # error message.
 pip install $(cat requirements.txt $(find lib -name requirements.txt | sort) | sort | uniq | sed 's/ *== */==/g')
-
